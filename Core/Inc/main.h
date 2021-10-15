e891ebc324e3c2b139647e20391b3b839afe121f
ahmad rofiq <ahmad.subsandi@viewtrixtech.com>
2021-10-15 14:50:48 +0700

    change UART 3 Pins

--- Core/Inc/main.h
+++ Core/Inc/main.h
@@ -62,10 +62,10 @@ void Error_Handler(void);
 #define LD3_GPIO_Port 				GPIOB
 
 
-#define STLK_RX_Pin 				GPIO_PIN_8
-#define STLK_RX_GPIO_Port 			GPIOD
-#define STLK_TX_Pin 				GPIO_PIN_9
-#define STLK_TX_GPIO_Port 			GPIOD
+#define STLK_RX_Pin 				GPIO_PIN_10
+#define STLK_RX_GPIO_Port 			GPIOB
+#define STLK_TX_Pin 				GPIO_PIN_11
+#define STLK_TX_GPIO_Port 			GPIOB
 #define USB_PowerSwitchOn_Pin 		GPIO_PIN_6
 #define USB_PowerSwitchOn_GPIO_Port GPIOG
 #define USB_OverCurrent_Pin 		GPIO_PIN_7
